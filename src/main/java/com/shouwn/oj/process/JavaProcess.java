ec08294e775fb9c7581840e072d158fffaa12b75
yejilee <yjll1019@naver.com>
2019-05-04 19:46:44 +0900

    process 생성자 구현.

--- src/main/java/com/shouwn/oj/process/JavaProcess.java
+++ src/main/java/com/shouwn/oj/process/JavaProcess.java
@@ -1,4 +1,16 @@
 package com.shouwn.oj.process;
 
-public class JavaProcess implements Process {
+import com.shouwn.oj.compile.JavaCompiler;
+import com.shouwn.oj.model.request.process.ProcessRequest;
+import com.shouwn.oj.run.JavaRunner;
+import com.shouwn.oj.sourceFile.JavaSourceFile;
+
+public class JavaProcess extends Process {
+
+	public JavaProcess(ProcessRequest processRequest) {
+		super(processRequest);
+		complier = new JavaCompiler();
+		runner = new JavaRunner();
+		sourceFile = new JavaSourceFile(sourceCode, pk);
+	}
 }
--- src/main/java/com/shouwn/oj/process/Process.java
+++ src/main/java/com/shouwn/oj/process/Process.java
@@ -1,4 +1,32 @@
 package com.shouwn.oj.process;
 
-public interface Process {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shouwn.oj.compile.Complier;
+import com.shouwn.oj.model.entity.problem.TestCase;
+import com.shouwn.oj.model.request.process.ProcessRequest;
+import com.shouwn.oj.run.Runner;
+import com.shouwn.oj.sourceFile.SourceFile;
+
+public abstract class Process {
+	protected String sourceCode;
+	protected String language;
+	protected List<TestCase> testCases;
+	protected long pk;
+	protected Complier complier;
+	protected Runner runner;
+	protected SourceFile sourceFile;
+
+	public Process(ProcessRequest processRequest) {
+		this.sourceCode = processRequest.getSourceCode();
+		this.language = processRequest.getLanguage();
+		this.testCases = processRequest.getTestcases();
+		this.pk = processRequest.getPk();
+	}
+
+	public List<String> run() {
+		List<String> result = new ArrayList<>();
+		return result;
+	}
 }
