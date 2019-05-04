b998e8a2b857f6b17100b8691cd5981e55b059ac
yejilee <yjll1019@naver.com>
2019-05-04 19:47:56 +0900

    process factory 기능 구현.

--- src/main/java/com/shouwn/oj/factory/ProcessFactory.java
+++ src/main/java/com/shouwn/oj/factory/ProcessFactory.java
@@ -1,4 +1,20 @@
 package com.shouwn.oj.factory;
 
+import com.shouwn.oj.model.request.process.ProcessRequest;
+import com.shouwn.oj.process.JavaProcess;
+import com.shouwn.oj.process.Process;
+import lombok.Getter;
+
+@Getter
 public class ProcessFactory {
+	private Process process;
+
+	public ProcessFactory(String lang, ProcessRequest processRequest) {
+		switch (lang){
+			case "java" :
+				process = new JavaProcess(processRequest);
+			default :
+				process = null;
+		}
+	}
 }
