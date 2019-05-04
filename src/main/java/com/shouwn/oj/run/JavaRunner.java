b295f9a3dc51706cdab8f3517d4f29eadc122c27
HYE_JI <lwiiiiii@naver.com>
2019-05-04 20:00:30 +0900

    process runner 기능 구현

--- src/main/java/com/shouwn/oj/run/JavaRunner.java
+++ src/main/java/com/shouwn/oj/run/JavaRunner.java
@@ -1,4 +1,8 @@
 package com.shouwn.oj.run;
 
-public class JavaRunner implements Runner {
+public class JavaRunner extends Runner {
+
+	public JavaRunner(String packageName, String fileName, String directoryPath) {
+		super(new String[] { "java", packageName + fileName }, directoryPath);
+	}
 }
--- src/main/java/com/shouwn/oj/run/Runner.java
+++ src/main/java/com/shouwn/oj/run/Runner.java
@@ -1,4 +1,49 @@
 package com.shouwn.oj.run;
 
-public interface Runner {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.shouwn.oj.model.entity.problem.TestCase;
+import com.shouwn.oj.util.StringUtils;
+
+public abstract class Runner {
+
+	protected String[] command;
+	protected String directoryPath;
+
+	public Runner(String[] command, String directoryPath) {
+		this.command = command;
+		this.directoryPath = directoryPath;
+	}
+
+	public List<String> run(List<TestCase> testCases) throws IOException {
+		List<String> results = new ArrayList<>();
+
+		ProcessBuilder processBuilder = new ProcessBuilder(command);
+		processBuilder.directory(new File(directoryPath));
+
+		Process process;
+
+		BufferedWriter stdIn;
+		BufferedReader stdOut;
+		BufferedReader stdErr;
+
+		for(int i = 0; i < testCases.size(); ++i) {
+			process = processBuilder.start();
+
+			stdIn = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+			stdOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			stdErr = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+			stdIn.write(testCases.get(i).getParams());
+			stdIn.newLine();
+			stdIn.flush();
+
+			results.add(Optional.of(StringUtils.inputStreamToString(stdOut)).orElse(StringUtils.inputStreamToString(stdErr)));
+		}
+
+		return results;
+	}
 }
