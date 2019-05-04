d3ceef3d1a424e93a9cb07872ea3c2227658c6a4
HYE_JI <lwiiiiii@naver.com>
2019-05-04 20:33:25 +0900

    JavaRunnerTest class 생성

---
+++ src/test/java/com/shouwn/oj/run/JavaRunnerTest.java
@@ -0,0 +1,16 @@
+package com.shouwn.oj.run;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class JavaRunnerTest {
+
+	@Test
+	public void fuc() {
+
+	}
+}
