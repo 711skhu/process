d887f4907a1c90895c75ddd5b9f1a1a656da10a6
HYE_JI <lwiiiiii@naver.com>
2019-05-05 20:54:53 +0900

    JavaProcess test 구현

---
+++ src/test/java/com/shouwn/oj/process/JavaProcessTest.java
@@ -0,0 +1,88 @@
+package com.shouwn.oj.process;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shouwn.oj.factory.ProcessFactory;
+import com.shouwn.oj.model.entity.member.Admin;
+import com.shouwn.oj.model.entity.problem.Course;
+import com.shouwn.oj.model.entity.problem.Problem;
+import com.shouwn.oj.model.entity.problem.ProblemDetail;
+import com.shouwn.oj.model.entity.problem.TestCase;
+import com.shouwn.oj.model.request.process.ProcessRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.shouwn.oj.model.enums.ProblemType.HOMEWORK;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class JavaProcessTest {
+
+	private ProcessRequest processRequest;
+	private Admin professor;
+	private Course newCourse;
+	private Problem problem;
+	private ProblemDetail problemDetail;
+	private TestCase testCase;
+	private List<TestCase> testCases;
+
+	@BeforeEach
+	void init() {
+		this.professor = Admin.builder()
+				.username("junit_tester")
+				.password("test123")
+				.name("tester")
+				.email("test@gmail.com")
+				.build();
+
+		this.newCourse = Course.builder()
+				.name("junit_test")
+				.description("test")
+				.enabled(true)
+				.professor(professor)
+				.build();
+
+		this.problem = Problem.builder()
+				.type(HOMEWORK)
+				.title("junit_test")
+				.course(newCourse)
+				.build();
+
+
+		this.problemDetail = ProblemDetail.builder()
+				.title("junit_test")
+				.content("junit-test")
+				.sequence(1)
+				.problem(problem)
+				.build();
+
+		this.testCase = TestCase.builder()
+				.params("test")
+				.result("test")
+				.problemDetail(problemDetail)
+				.build();
+
+		this.testCases = new ArrayList<>();
+		testCases.add(testCase);
+
+		this.processRequest = ProcessRequest.builder()
+				.language("java")
+				.pk(1)
+				.sourceCode("public class Main {}")
+				.testCases(testCases)
+				.build();
+	}
+
+	@Test
+	public void javaProcessConstructorTestSuccess() {
+		Process process = new ProcessFactory(processRequest).getProcess();
+
+		Assertions.assertEquals(true, process.run() instanceof List);
+	}
+
+}
