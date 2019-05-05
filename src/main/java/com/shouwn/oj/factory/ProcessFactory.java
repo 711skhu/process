7dc54216b06f557dfe8bcc911998c250c1f9a32a
yejilee <yjll1019@naver.com>
2019-05-05 19:03:26 +0900

    ProcessFactory클래스 TODO추가.

--- src/main/java/com/shouwn/oj/factory/ProcessFactory.java
+++ src/main/java/com/shouwn/oj/factory/ProcessFactory.java
@@ -10,6 +10,7 @@ public class ProcessFactory {
 
 	private Process process;
 
+	// TODO 생성자 예외 발생 시 처리.
 	public ProcessFactory(ProcessRequest processRequest) {
 		switch(processRequest.getLanguage()) {
 			case "java":
